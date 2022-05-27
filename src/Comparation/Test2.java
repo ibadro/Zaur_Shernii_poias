eb44d6460a930ba096b2ba72dcdd7e43c43df5c0
i.badretdinov <ilnurb@mail.ru>
2022-05-27 06:38:00 +0500

    !!!

--- src/Comparation/Test2.java
+++ src/Comparation/Test2.java
@@ -1,7 +1,19 @@
 package Comparation;
 
-public class Test2 {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Test2 {
+    public static void main(String[] args) {
+        List <Employee> List = new ArrayList<>();
+        Employee emp1 = new Employee( 100, "zaur","tregulov", 134);
+        Employee emp2 = new Employee( 120, "ilnur","badro", 1234);
+        Employee emp3 = new Employee( 130, "za ","trov", 1123233);
+        List.add(emp1);
+        List.add(emp2);
+        List.add(emp3);
+        System.out.println("Before sorting \n" + List);
+    }
 }
 class Employee{
     int id;
